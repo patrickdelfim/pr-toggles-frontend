63979962036312969a332c28d454b348bdd92bfc
Patrick Delfim <patrickdelfim@gmail.com>
2022-10-02 00:50:37 -0300

    feat: skeletonCardList component

---
+++ src/presentation/pages/projects/Components/skeletonCardList.tsx
@@ -0,0 +1,26 @@
+import { Box, Skeleton, Stack } from '@chakra-ui/react'
+import React from 'react'
+
+const SkeletonCardList: React.FC = () => {
+  return (
+    <Box>
+      {[...Array(5)].map((_, idx) => (
+        <Box
+          key={idx}
+          bg="white"
+          minW={'full'}
+          minH="60px"
+          borderBottom="1px"
+          borderColor="gray.200"
+        >
+          <Stack p="4">
+            <Skeleton height="10px" />
+            <Skeleton height="10px" />
+          </Stack>
+        </Box>
+      ))}
+    </Box>
+  )
+}
+
+export default SkeletonCardList
