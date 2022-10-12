314174ea4623aeb08acda0f782ce37426af62216
Patrick Delfim <patrickdelfim@gmail.com>
2022-10-12 10:13:54 -0300

    feat: create new color schema for links

--- src/presentation/styles/components/linkStyles.ts
+++ src/presentation/styles/components/linkStyles.ts
@@ -4,15 +4,22 @@ export const LinkTheme: ComponentStyleConfig = {
   baseStyle: {},
   sizes: {},
   variants: {
-    primary: {
+    secondary: {
       color: 'secondary.500',
       fontWeight: '700',
       _focus: { border: 'none' },
       _hover: { color: 'secondary.400' },
       _active: { color: 'secondary.300' },
     },
+    primary: {
+      color: 'primary.500',
+      fontWeight: '700',
+      _focus: { border: 'none' },
+      _hover: { color: 'primary.400' },
+      _active: { color: 'primary.300' },
+    },
   },
   defaultProps: {
-    variant: 'primary',
+    variant: 'secondary',
   },
 }
