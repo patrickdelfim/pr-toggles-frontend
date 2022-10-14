0a50309e6cdda487d7295d4096650607065b83f0
Patrick Delfim <patrickdelfim@gmail.com>
2022-10-13 21:57:48 -0300

    feat: adjust tests

--- src/main/test/cypress/integration/private-routes.spec.ts
+++ src/main/test/cypress/integration/private-routes.spec.ts
@@ -4,7 +4,7 @@ describe('Private Routes', () => {
   it('should logout if protected routes has no token', () => {
     cy.visit('/panel')
     Helper.testUrl('/login')
-    cy.visit('/panel/project')
+    cy.visit('/panel/project/2')
     Helper.testUrl('/login')
   })
 })
