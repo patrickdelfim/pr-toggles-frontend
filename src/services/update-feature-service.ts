45aa3c1898be150369bf44b811c11f8c628d47d1
Patrick Delfim <patrickdelfim@gmail.com>
2022-10-15 16:49:06 -0300

    feat: update feature use case implementation

---
+++ src/services/update-feature-service.ts
@@ -0,0 +1,30 @@
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
+import { UpdateFeature } from '@/domain/usecases/update-feature'
+import { HttpStatusCode, makeApiUrl, makeRequest } from './api-service'
+
+export default class UpdateFeatureService implements UpdateFeature {
+  async update (params: UpdateFeature.params): Promise<UpdateFeature.Model> {
+    const httpResponse = await makeRequest({
+      url: makeApiUrl(`/api/features/${params.feature_id}`),
+      method: 'patch',
+      body: params,
+    })
+    const feature = httpResponse.body?.feature
+    console.log('updated feature: ', feature)
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        return Object.assign(feature, {
+          created_at: new Date(feature.created_at).toLocaleDateString(
+            'pt-BR'
+          ),
+          updated_at: new Date(feature.updated_at).toLocaleDateString(
+            'pt-BR'
+          )
+        })
+      case HttpStatusCode.forbidden:
+        throw new AccessDeniedError()
+      default:
+        throw new UnexpectedError()
+    }
+  }
+}
