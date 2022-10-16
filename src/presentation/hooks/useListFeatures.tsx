08e20c70b10f3692a32e55a434afe0a2cf5b8114
Patrick Delfim <patrickdelfim@gmail.com>
2022-10-16 13:42:41 -0300

    fix: adjust composed useQuery keys to prevend data type bug

--- src/presentation/hooks/useListFeatures.tsx
+++ src/presentation/hooks/useListFeatures.tsx
@@ -2,10 +2,9 @@ import { FeatureModel } from '@/domain/models/feature-model'
 import LoadFeatureService from '@/services/load-feature-service'
 import { useQuery } from 'react-query'
 
-function useListFeatures (projectId, onError?): any {
+function useListFeatures (projectId: string, onError?): any {
   const loadFeatureService = new LoadFeatureService()
-  console.log('loading features')
-  return useQuery<FeatureModel[], Error>(['features', projectId], async () => await loadFeatureService.loadByProjectId(projectId), {
+  return useQuery<FeatureModel[], Error>(['features', `${projectId}`], async () => await loadFeatureService.loadByProjectId(projectId), {
     onError
   })
 }
--- src/presentation/hooks/usegetProject.tsx
+++ src/presentation/hooks/usegetProject.tsx
@@ -2,10 +2,10 @@ import { ProjectModel } from '@/domain/models'
 import LoadProjectsService from '@/services/load-project-service'
 import { useQuery } from 'react-query'
 
-function useGetProject (projectId, initialData?, onError?): any {
+function useGetProject (projectId: string, initialData?, onError?): any {
   const loadProjectsService = new LoadProjectsService()
 
-  return useQuery<ProjectModel, Error>(['projects', projectId], async () => await loadProjectsService.loadById(projectId), {
+  return useQuery<ProjectModel, Error>(['projects', `${projectId}`], async () => await loadProjectsService.loadById(projectId), {
     initialData,
     onError
   })
